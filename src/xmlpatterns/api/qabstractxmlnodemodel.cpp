4a6ce5c34b65df043df36c46ba03a45e36ff0c95
Sergio Martins <sergio.martins@kdab.com>
2015-05-28 12:34:03 +0100

    Use QVector::reserve()
    Change-Id: I86391bf0ede4fb32037b4e98889650e36ef5d279
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>

--- src/xmlpatterns/api/qabstractxmlnodemodel.cpp
+++ src/xmlpatterns/api/qabstractxmlnodemodel.cpp
@@ -483,8 +483,9 @@ namespace QPatternist
         virtual QAbstractXmlForwardIterator<QXmlNodeModelIndexIteratorPointer>::Ptr copy() const
         {
             ItVector result;
-
-            for(int i = 0; i < m_list.count(); ++i)
+            const int count = m_list.count();
+            result.reserve(count);
+            for (int i = 0; i < count; ++i)
                 result.append(m_list.at(i)->copy());
 
             return Ptr(new IteratorVector(result));
@@ -501,6 +502,7 @@ static inline QXmlNodeModelIndexIteratorPointer mergeIterators(const QXmlNodeMod
                                                                const QXmlNodeModelIndexIteratorPointer &it2)
 {
     QVector<QXmlNodeModelIndexIteratorPointer> iterators;
+    iterators.reserve(2);
     iterators.append(makeSingletonIterator(node));
     iterators.append(it2);
 
